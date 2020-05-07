58ef1e6f32729d8009ecafd1aaa97edb93911a9e
Olei Eto <teneeto@gmail.com>
2020-05-08 00:53:47 +0100

    visual polish:: complete

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,13 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter (Activity context, ArrayList<Word> words){
+
+    private int mColorResourceId;
+
+
+    public WordAdapter (Activity context, ArrayList<Word> words, int colorResourceId){
         super(context,0,words);
+        mColorResourceId = colorResourceId;
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -38,6 +44,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         }else{
             imageView.setVisibility(View.GONE);
         }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
 
         return listItemView;
